4ddcdcc2a36f554370b1cd7a507bc99e632a5529
Oxana <oxana@PC-05>
2016-07-25 13:25:56 +0200

    Funciona pero sin elección de desglose. Con enteros otra vez.

--- main.c
+++ main.c
@@ -4,160 +4,138 @@
 
 int main() {
 
-    float cF;
-    //int resto, resto500, resto200, resto100, resto50, resto20, resto10, resto5, resto2, resto1,cE, cP;
-    float resto, resto500, resto200, resto100, resto50, resto20, resto10, resto5, resto2, resto1, cE;
-    float cP;
-    //importe2=(int)importe;
-    /*
+    int resto, resto200, resto100, resto50, resto20, resto10, resto5, resto2, resto1, resto050, resto020, resto010, resto05, resto02, resto01 ;
+    float cTotal;
+    int cEntera, cDecimal;
+    
     printf("Ingrese una cantidad\n");
-    scanf("%f", &cF);
-    printf("Ha introducido %0.2f\n", cE);*/
- 
+    scanf("%f", &cTotal);
+    printf("Ha introducido %0.2f\n", cTotal);
     
-    
-    //cP=cE;
-    cP=1470.30;
-    
-    printf("> %f \n", cP);
-    resto=(cP / 500);
-    if (resto != 0){
-        printf("500> %0.0f \n", resto);
+    cEntera=cTotal;
+    cDecimal=(cTotal-cEntera)*100;
+/*
+    printf("Billete a empezar \n");
+    scanf("%d", &billete);
+    if (billete == 500)
+    {
+        printf("Quiere usar un billete de 500\n");
     }
-    resto=fmodf(cP,500);
+    else if (billete == 200)
+    {
+        printf("Quiere usar un billete de 200\n");
+    }
+    else if (billete == 100)
+    {
+        printf("Quiere usar un billete de 100\n");
+    }
+   
+*/
 
-                                                    printf("resto: %0.2f \n", resto);
+    resto=(cEntera / 500);
+    if (resto != 0){
+        printf("500> %i \n", resto);
+    }
+    resto=(cEntera % 500);
+                                                   // printf("resto: %i \n", resto);
  
     resto200=(resto / 200);
     if (resto200 != 0){
-        printf("200> %0.0f \n", resto200);
+        printf("200> %i \n", resto200);
     }
-    resto=fmodf(resto,200);
-                                                    printf("resto: %0.2f \n", resto);
+    resto=(resto % 200);
+                                                    //printf("resto: %i \n", resto);
     resto100=(resto / 100);
     if (resto100 != 0){
-        printf("100> %0.0f \n", resto100);
+        printf("100> %i \n", resto100);
     }
-    resto=fmodf(resto,100);
-                                                    printf("resto: %0.2f \n", resto);
+    resto=(resto % 100);
+                                                    //printf("resto: %i \n", resto);
     
     resto50=(resto / 50);
     if (resto50 != 0){
-        printf("50> %0.0f \n", resto50);
+        printf("50> %i \n", resto50);
     }
-    resto=fmodf(resto,50);
-                                                    printf("resto> %0.2f \n", resto);
+    resto=(resto % 50);
+                                                    //printf("resto> %i \n", resto);
 
     resto20=(resto / 20);
     if (resto20 != 0){
-        printf("20> %0.0f \n", resto20);
+        printf("20> %i \n", resto20);
     }
-    resto=fmodf(resto,20);
-                                                    printf("resto> %0.2f \n", resto);
+    resto=(resto % 20);
+                                                    //printf("resto> %i \n", resto);
     resto10=(resto / 10);
     if (resto10 != 0){
-        printf("10> %0.0f \n", resto);
+        printf("10> %i \n", resto10);
     }
-    resto=fmodf(resto,10);
-                                                    printf("resto> %0.2f \n", resto);
+    resto=(resto % 10);
+                                                    //printf("resto> %i \n", resto);
     
     resto5=(resto / 5);
     if (resto5 != 0){
-        printf("5> %0.0f \n", resto);
+        printf("5> %i \n", resto5);
     }
-    resto=fmodf(resto,5);
-                                                    printf("resto> %0.2f \n", resto);
+    resto=(resto % 5);
+                                                   // printf("resto> %i \n", resto);
+    
     resto2=(resto / 2);
     if (resto2 != 0){
-        printf("2> %0.0f \n", resto);
+        printf("2> %i \n", resto2);
     }
-    resto=fmodf(resto,2);
-                                                    printf("resto> %0.2f \n", resto);
+    resto=(resto % 2);
+                                                    //printf("resto> %i \n", resto);
     
     resto1=(resto / 1);
     if (resto1 != 0){
-        printf("1> %0.0f \n", resto);
+        printf("1> %i \n", resto1);
     }
-    resto=fmodf(resto,1);
-                                                    printf("resto> %0.2f \n", resto);
+    resto=(resto % 1);
+                                                    //printf("resto> %i \n", resto);
     
-    //DECIMALES
+
+    //DECIMALES________________________________________________________________________________
+    resto=(cDecimal / 50);
     if (resto != 0){
-        printf("0.50> %0.0f \n", resto);
+        printf("0.50> %i \n", resto);
     }
-    resto=fmodf(resto,0.50);
-                                                printf("resto> %0.2f \n", resto);
-    
-    if (resto != 0){
-        printf("0.20> %0.0f \n", resto);
+    resto=(cDecimal % 50);
+                                                //printf("resto> %i \n", resto);
+
+    resto020=(resto / 20);
+    if (resto020 != 0){
+        printf("0.20> %i \n", resto020);
     }
-    resto=fmodf(resto,0.20);
-    printf("resto> %0.2f \n", resto);
-    
-    if (resto != 0){
-        printf("0.10> %0.0f \n", resto);
+    resto=(resto % 20);
+                                                    //printf("resto> %i \n", resto);
+    resto010=(resto / 10);
+    if (resto010 != 0){
+        printf("0.10> %i \n", resto10);
     }
-    resto=fmodf(resto,0.10);
-    printf("resto> %0.2f \n", resto);
-    
-    if (resto != 0){
-        printf("0.05> %0.0f \n", resto);
+    resto=(resto % 10);
+                                                    //printf("resto> %i \n", resto);
+                                                   
+    resto05=(resto / 5);
+    if (resto05 != 0){
+        printf("0.05> %i \n", resto05);
     }
-    resto=fmodf(resto,0.05);
-    printf("resto> %0.2f \n", resto);
+    resto=(resto % 5);
     
-    if (resto != 0){
-        printf("0.02> %0.0f \n", resto);
+                                                    //printf("resto> %i \n", resto);
+    resto02=(resto / 2);
+    if (resto02 != 0){
+        printf("0.02> %i \n", resto02);
     }
-    resto=fmodf(resto,0.02);
-    printf("resto> %0.2f \n", resto);
+    resto=(resto % 2);
+                                                        //printf("resto> %i \n", resto);
     
-    if (resto != 0){
-        printf("0.01> %0.0f \n", resto);
+    resto01=(resto / 1);
+    if (resto01 != 0){
+        printf("0.01> %i \n", resto01);
     }
-    resto=fmodf(resto,0.01);
-    printf("resto> %0.2f \n", resto);
-    
+    resto=(resto % 1);
+                                                        //printf("resto> %i \n", resto);
     
     return 0;
-    
-    
-/*
-$c=1895.09;
-$cp=$c;
-
-$b500=(int)($cp/500);
-$cp=$cp-$b500*500;
-$b200=(int)($cp/200);
-$cp=$cp-$b200*200;
-$b100=(int)($cp/100);
-$cp=$cp-$b100*100;
-$b50=(int)($cp/50);
-$cp=$cp-$b50*50;
-$b20=(int)($cp/20);
-$cp=$cp-$b20*20;
-$b10=(int)($cp/10);
-$cp=$cp-$b10*10;
-$b5=(int)($cp/5);
-$cp=$cp-$b5*5;
-$m2=(int)($cp/2);
-$cp=$cp-$m2*2;
-$m1=(int)($cp/1);
-$cp=$cp-$m1*1;
-$m050=(int)($cp/0.50);
-$cp=$cp-$m050*0.50;
-$m020=(int)($cp/0.20);
-$cp=$cp-$m020*0.20;
-$m010=(int)($cp/0.10);
-$cp=$cp-$m010*0.10;
-$m005=(int)($cp/0.05);
-$cp=$cp-$m005*0.05;
-$m002=(int)($cp/0.02);
-$cp=$cp-$m002*0.02;
-$m001=(int)($cp/0.01);
-$cp=$cp-$m001*0.01;
-echo "La cantidad $c necesita $b500 de 500, $b200 de 200, $b100 de 100, $b50 de 50, $b20 de 20, $b10 de 10, $b5 de 5, y monedas $m2 de 2, $m1 de 1, $m050 de 0.50, $m020 de 0.20, $m010 de 0.10, $m005 de 0.05, $m002 de 0.02 y $m001 de 0.01.";
-
-*/
 }
 
