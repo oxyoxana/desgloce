78f74a7443a40e049c9b3c504460c971d915db6b
Oxana <oxana@PC-05>
2016-07-25 09:18:48 +0200

    Está mal

--- main.c
+++ main.c
@@ -4,43 +4,116 @@
 int main() {
 
     float cF;
-    int resto, resto500, resto200, cE;
+    int resto, resto500, resto200, cE, cP;
     //importe2=(int)importe;
-    
+    /*
     printf("Ingrese una cantidad\n");
     scanf("%f", &cF);
-    printf("Ha introducido %0.2f\n", cE);
+    printf("Ha introducido %0.2f\n", cE);*/
  
     
     
     //cP=cE;
-    cP=1700;
+    cP=1701;
     
-    
-    printf("> %i \n", cE);
-    resto500=(cE / 500);
+   
+    printf("> %i \n", cP);
+    resto500=(cP / 500);
     if (resto500 != 0){
         printf("500> %i \n", resto500);
     }
-    resto=(cE % 500);
+    resto=(cP % 500);
     
     if (resto != 0){
         printf("200> %i \n", resto);
     }
-    resto=(cE % 200);
+    resto=(cP % 200);
     
     if (resto != 0){
         printf("100> %i \n", resto);
     }
-    resto=(cF % 100);
+    resto=(cP % 100);
     printf("resto> %i \n", resto);
     
+    if (resto != 0){
+        printf("50> %i \n", resto);
+    }
+    resto=(cP % 50);
+    printf("resto> %i \n", resto);
+
+    if (resto != 0){
+        printf("20> %i \n", resto);
+    }
+    resto=(cP % 20);
+    printf("resto> %i \n", resto);
+    
+    if (resto != 0){
+        printf("10> %i \n", resto);
+    }
+    resto=(cP % 10);
+    printf("resto> %i \n", resto);
+    
+    if (resto != 0){
+        printf("5> %i \n", resto);
+    }
+    resto=(cP % 5);
+    printf("resto> %i \n", resto);
+    
+    if (resto != 0){
+        printf("2> %i \n", resto);
+    }
+    resto=(cP % 2);
+    printf("resto> %i \n", resto);
+    
+    if (resto != 0){
+        printf("1> %i \n", resto);
+    }
+    resto=(cP % 1);
+    printf("resto> %i \n", resto);
+    
+    /*
+    if (resto != 0){
+        printf("0.50> %i \n", resto);
+    }
+    resto=(cF % 0.50);
+    printf("resto> %i \n", resto);
+    
+    if (resto != 0){
+        printf("0.20> %i \n", resto);
+    }
+    resto=(cF % 0.20);
+    printf("resto> %i \n", resto);
+    
+    if (resto != 0){
+        printf("0.10> %i \n", resto);
+    }
+    resto=(cF % 0.10);
+    printf("resto> %i \n", resto);
+    
+    if (resto != 0){
+        printf("0.05> %i \n", resto);
+    }
+    resto=(cF % 0.05);
+    printf("resto> %i \n", resto);
+    
+    if (resto != 0){
+        printf("0.02> %i \n", resto);
+    }
+    resto=(cF % 0.02);
+    printf("resto> %i \n", resto);
+    
+    if (resto != 0){
+        printf("0.01> %i \n", resto);
+    }
+    resto=(cF % 0.01);
+    printf("resto> %i \n", resto);
+     */
     
     return 0;
     
     
 /*
-    $c=1895.09;
+$c=1895.09;
 $cp=$c;
 
 $b500=(int)($cp/500);
@@ -74,7 +147,7 @@ $cp=$cp-$m002*0.02;
 $m001=(int)($cp/0.01);
 $cp=$cp-$m001*0.01;
 echo "La cantidad $c necesita $b500 de 500, $b200 de 200, $b100 de 100, $b50 de 50, $b20 de 20, $b10 de 10, $b5 de 5, y monedas $m2 de 2, $m1 de 1, $m050 de 0.50, $m020 de 0.20, $m010 de 0.10, $m005 de 0.05, $m002 de 0.02 y $m001 de 0.01.";
-?>
+
 */
 }
 
