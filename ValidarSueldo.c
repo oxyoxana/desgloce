b83b13a7ac45c74472287483b379e988ecc705db
Oxana <oxana@PC-05>
2016-07-28 11:11:06 +0200

    Mejorado con decimales. Se retorna num, billete, cEntera(parte entera de num) y cDecimal(parte decimal de num).

--- ValidarSueldo.c
+++ ValidarSueldo.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /*
  * Validar el sueldo introducido y elegir 
  * el tipo de billete máximo a utilizar (500,200,100 ó 50).
@@ -9,47 +8,63 @@
 
 int validarSueldo() {
 
-    int num;
+    int cEntera, cDecimal;
     int billete;
     int billeteV;
     char stringNum[5];
-
-    do {
+    float num;
+    
+    do{
         printf("Ingrese una cantidad\n");
-        scanf("%s", stringNum);
-        num = atoi(stringNum);
+        scanf("%s",stringNum);
+        num=atof(stringNum);
         printf("Billete a empezar \n");
         scanf("%d", &billete);
-
-        if (billete == 500) {
-            billeteV = 1;
-        } else if (billete == 200) {
-            billeteV = 1;
-        } else if (billete == 100) {
-            billeteV = 1;
-        } else if (billete == 50) {
-            billeteV = 1;
-        } else {
-            printf("Billete no válido.\n");
-            billeteV = 0;
-        }
-    }    while ((num < 1) || (billeteV != 1) || (validarEntrada(stringNum) != 0));
-    if (num != 0) {
-        printf("El numero es: %d\n", num);
+                
+            if (billete == 500) {
+                billeteV = 1;
+            } else if (billete == 200) {
+                billeteV = 1;
+            } else if (billete == 100) {
+                billeteV = 1;
+            } else if (billete == 50) {
+                billeteV = 1;
+            } else {
+                printf("Billete no válido.\n");
+                billeteV = 0;
+            }
     }
-    return num, billete;
+    while ((num<1) || (billeteV != 1) || (validarEntrada(stringNum)!=0));
+        if(num!=0){
+            //printf("El numero es: %f\n",num);
+            printf("el número introducido es %f y el billete es %i\n", num, billete);
+            cEntera= (int)num;
+            //printf("entera %i\n", cEntera);
+            cDecimal=(num-cEntera)*100;
+            //printf("decimal %i\n", cDecimal);
+        }
+    
+    return num, billete, cEntera, cDecimal;
 }
 
-int validarEntrada(char nom[]) {
-    int i = 0;
-    int valido = 0;
-    int numCarac = strlen(nom);
 
-    for (i = 0; i < numCarac; i++) {
-        if (!isdigit(nom[i])) {
+int validarEntrada(char nom[]) 
+{ 
+    int i = 0; 
+    int valido = 0;
+    int numCarac = strlen(nom); 
+
+    for( i = 0; i < numCarac; i++) 
+    { 
+        if(isalpha(nom[i])) 
+        {  
             valido = 1;
+            break;
+        }else if(isdigit(nom[i])) 
+        {  
+            valido = 0;
             break;
         }
     }
-    return valido;
+    return valido; 
 }
