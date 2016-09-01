f86cad659552b1f029443e892d3e57479f3bb9f7
Oxana <oxana@PC-05>
2016-09-01 13:56:01 +0200

    con imperfecciones menores

--- ValidarSueldo.c
+++ ValidarSueldo.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <math.h>
 /*
  * Validar el sueldo introducido y elegir 
  * el tipo de billete máximo a utilizar (500,200,100 ó 50).
@@ -9,11 +10,9 @@
 
 int main() {
 
-    int cEntera, cDecimal, resto, restoSobrante;
-    int billete, euro;
-    int billeteV;
+    int cEntera, cDecimal, billete, billeteV;
     char stringNum[5];
-    float num;
+    float num, nearest;
     int x[] = {500, 200, 100, 50, 20, 10, 5, 2, 1};
     int z[] = {50, 20, 10, 5, 2, 1};
     
@@ -23,7 +22,7 @@ int main() {
         num=atof(stringNum);
         printf("Billete a empezar: \n");
         scanf("%d", &billete);
-                
+            
             if (billete == 500) {
                 billeteV = 1;
             } else if (billete == 200) {
@@ -37,22 +36,19 @@ int main() {
                 billeteV = 0;
             }
     }
-    while ((num<1) || (billeteV != 1) || (validarEntrada(stringNum)!=0));
+    while ((num<1) || (billeteV != 1) || (validarEntrada(stringNum)!=0) || (num < billete));
         if(num!=0){
-            //printf("El numero es: %f\n",num);
             printf("El número introducido es %f y el billete es %i\n", num, billete);
             cEntera= (int)num;
             //printf("entera %i\n", cEntera);
-            cDecimal=(num-cEntera)*100;
-            //printf("decimal %i\n", cDecimal);
+            cDecimal=ceil((num-cEntera)*100);
+            printf("decimal %i\n", cDecimal);
             
             //resto=(cEntera / billete);
             //printf("resto %d \n", resto);
             
-            //monedas=cDecimal/;
-            
             //Calculo billete______________________________________________
-            int i, j, posicionBillete, posicionMonedas, cantidadEuro, cantidadDecimal, cantidadEntera, monedas;
+            int i, posicionBillete, cantidadEntera;
             for (i = 0; i < 4; i++) {
                 if (x[i]==billete)
                     posicionBillete=i;
@@ -69,14 +65,18 @@ int main() {
                 cantidadEntera=cEntera / x[i];
                 cEntera = cEntera - (cantidadEntera * x[i]);
                 if (cantidadEntera > 0)
-                    printf("%d billetes de %d €\n", cantidadEntera, x[i]);
+                    if (i<7){
+                        printf("%d billetes de %d €\n", cantidadEntera, x[i]);
+                    } else {
+                        printf("%d monedas de %d €\n", cantidadEntera, x[i]);
+                    }
             }
             
             //Calculo monedas______________________________________________
+            int j, posicionMonedas, cantidadDecimal;
             for (j = 0; j < 6; j++) {
                 if (z[j]==cDecimal){
                     posicionMonedas=j;
-                    printf("posicionm %d\n", j);
                 }
             }
             for (j=posicionMonedas; j<6; j++){
@@ -86,7 +86,6 @@ int main() {
                     printf("%d monedas de %d centimos\n", cantidadDecimal, z[j]);
             }
         }
-    
     return 0;
 }
 
