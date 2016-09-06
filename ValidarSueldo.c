6a200b09b8d82cd6453a715b1204c73ec609dd99
Oxana <oxana@PC-05>
2016-09-06 09:54:41 +0200

    100%

--- ValidarSueldo.c
+++ ValidarSueldo.c
@@ -14,8 +14,7 @@ int main() {
     int cEntera, cDecimal, billete, billeteV;
     char stringNum[5];
     float num;
-    int x[] = {500, 200, 100, 50, 20, 10, 5, 2, 1};
-    int z[] = {50, 20, 10, 5, 2, 1};
+    int euros[] = {500, 200, 100, 50, 20, 10, 5, 2, 1};
 
     do {
         printf("Ingrese una cantidad: \n");
@@ -41,45 +40,39 @@ int main() {
         //Calculo billete______________________________________________
         int i, posicionBillete, cantidadEntera;
         for (i = 0; i < 4; i++) {
-            if (x[i] == billete)
+            if (euros[i] == billete)
                 posicionBillete = i;
         }
-
         for (i = posicionBillete; i < 9; i++) {
-            cantidadEntera = cEntera / x[i];
-            cEntera = cEntera - (cantidadEntera * x[i]);
+            cantidadEntera = cEntera / euros[i];
+            cEntera = cEntera - (cantidadEntera * euros[i]);
             if (cantidadEntera > 0) {
                 if (i < 7) {
                     if (cantidadEntera == 1) {
-                        printf("%d billete de %d €\n", cantidadEntera, x[i]);
+                        printf("%d billete de %d €\n", cantidadEntera, euros[i]);
                     } else {
-                        printf("%d billetes de %d €\n", cantidadEntera, x[i]);
+                        printf("%d billetes de %d €\n", cantidadEntera, euros[i]);
                     }
                 } else {
                     if (cantidadEntera == 1) {
-                        printf("%d moneda de %d €\n", cantidadEntera, x[i]);
+                        printf("%d moneda de %d €\n", cantidadEntera, euros[i]);
                     } else {
-                        printf("%d monedas de %d €\n", cantidadEntera, x[i]);
+                        printf("%d monedas de %d €\n", cantidadEntera, euros[i]);
                     }
                 }
             }
         }
 
         //Calculo monedas______________________________________________
-        int j, posicionMonedas, cantidadDecimal;
-        for (j = 0; j < 6; j++) {
-            if (z[j] == cDecimal) {
-                posicionMonedas = j;
-            }
-        }
-        for (j = posicionMonedas; j < 6; j++) {
-            cantidadDecimal = cDecimal / z[j];
-            cDecimal = cDecimal - (cantidadDecimal * z[j]);
+        int j, cantidadDecimal;
+        for (j = 3; j < 9; j++) {
+            cantidadDecimal = cDecimal / euros[j];
+            cDecimal = cDecimal - (cantidadDecimal * euros[j]);
             if (cantidadDecimal > 0) {
                 if (cantidadDecimal == 1) {
-                    printf("%d moneda de %d céntimos\n", cantidadDecimal, z[j]);
+                    printf("%d moneda de %d céntimos\n", cantidadDecimal, euros[j]);
                 } else {
-                    printf("%d monedas de %d céntimos\n", cantidadDecimal, z[j]);
+                    printf("%d monedas de %d céntimos\n", cantidadDecimal, euros[j]);
                 }
             }
         }
